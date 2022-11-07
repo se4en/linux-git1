ab1d575bcb3723e20b42bf6a11e87893681df609
se4en <001122mj@gmail.com>
2022-11-07 16:36:58 +0000

    Add solution

---
+++ process.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+awk -F, '
+{
+	 sum += $18
+}
+END {
+	print "RATING_AVG", sum/NR
+}' $1;
+
+awk -F, '
+{
+	cnt[tolower($7)]++
+}
+END {
+	for (country in cnt) print "HOTELNUMBER", country, cnt[country]
+}' $1;
+
+awk -F, '
+{
+#	split($2, hotel_name, "/ /");
+	# print substr($2, 1, 7)
+#	print $2, hotel_name[0], tolower(hotel_name[0]) == "hilton";
+	if (substr($2, 1, 11)  == "holiday inn")
+	{
+		# print($1);
+		val_hinn[tolower($7)] += $12;
+		cnt_hinn[tolower($7)]++;
+	} else if (substr($2, 1, 6) == "hilton")
+	{
+		val_hilton[tolower($7)] += $12;
+		cnt_hilton[tolower($7)]++;
+	}
+}
+END {
+	for (country in val_hinn) print "CLEANLINESS" , country, val_hinn[country]/cnt_hinn[country], val_hilton[country]/cnt_hilton[country]
+}
+' $1;
+
+gnuplot -e "
+set datafile separator ',';
+set terminal png size 1000,1000;
+set output 'plot.png';
+f(x) = a*x + b;
+fit f(x) '$1' using 12:18 via a,b;
+plot f(x)"
